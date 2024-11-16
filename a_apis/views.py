2cab35617f40af151b3cd07de942248f66cfd6a1
bamjun <khy5116@naver.com>
2024-11-16 13:00:46 +0900

    jsonresponse 추가

--- a_apis/views.py
+++ a_apis/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema, Response
+from ninja.responses import JSONResponse
 
 api = NinjaAPI()
 
 
 @api.get("/test")
 def test(request):
-    return {"message": "Hello, World!"}
+    return JSONResponse({"message": "Hello, World!"})
